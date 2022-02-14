3392628efcf178fb26fa2fda47e352e00f224337
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-13 20:50:30 -0800

    finished preliminary code for Contact section in components/Contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -16,6 +16,20 @@ function Contact() {
     return (
         <section>
             <h1>Contact Me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type='text' defaultValue={name} onChange={handleChange} name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type='email' defaultValue={email} onChange={handleChange} name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" defaultValue={message} onChange={handlechange} rows="5" />
+                </div>
+            </form>
         </section>
     )
 }
