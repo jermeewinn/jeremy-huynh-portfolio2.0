1f8370d26b24a65e13b469cc6279211a06d863ec
Jeremy Huynh <jeahuynh18@gmail.com>
2022-02-28 15:08:58 -0800

    added logic to singular Project object in React

---
+++ src/components/Project/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import { BsGithub } from 'react-icons/bs';
+
+const Project = ({ project }) => {
+    return(
+        <div className='mx-2 my-1'>
+            <h4>{project.name}
+            <span id='icon' className='mx-5'><a href={project.git}><BsGithub /> GitHub Link</a></span></h4>
+            <a href={project.link}><img src={require(`../../assets/images/projects/${project.src}`)}
+                alt={project.name}
+                classname='img-thumbnail'
+                key={project.name} /></a>
+            <p>{project.description}</p>
+        </div>
+    );
+};
+
+export default Project;
