db1d9f8790a904b88f0dd829b93fadcfdfc08338
Jeremy Huynh <jeahuynh18@gmail.com>
2022-03-02 13:17:23 -0800

    adjusted JSX in About/index.js to align with CSS styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,15 +3,17 @@ import profilePic from '../../assets/images/other/Profile-Picture.jpg';
 
 function About() {
     return (
-        <section id="sideByside">
+        <section id='about'>
             <h1 id='about'>About Me</h1>
-            <img src={profilePic} style={{ float: "left", alignItems: "center", width: "30%" }} alt='portrait of Jeremy' />
-            <div style={{ float: "right", width: "50%", textalign: "left",  }}>
+            <div id='sideByside'>
+                <div id='me-image'>
+                <img id="me" src={profilePic} className="my-2" alt="potrait of Jeremy"/>                </div>
+            </div>
+            <div id='left'>
                 <p>
                     My name is Jeremy Huynh, and I am an alumni of UC Santa Cruz, class of 2018. 
                     The last few years, I've worked as a sales representative for a variety of startups around the Bay Area; 
-                    ranging from alternative business financing, to database catalogs and IoT security solutions.
-                    <br/>
+                    ranging from alternative business financing, to database catalogs and IoT security solutions.  
                     The breadth of my experiences speak to my ability in working in teams and on my own. 
                     If you have any questions or inquiries about my experiences, please feel free to reach out to me via the Contact Section.
                 </p>
